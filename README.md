74d8021c34feaba2901fa9149985a97cfebaa71b
Avrel <avinash3pkhrl@gmail.com>
2022-07-17 16:46:10 +0545

    !!!

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-This is a site in development for betterment of the life of noob programmers
-
--Thank you
+# Same as Always
+## Roaming around
---
+++ assets/script.js
@@ -0,0 +1,6 @@
+const width = screen.width;
+const height = screen.height;
+
+console.log(new Date());
+console.log(navigator.userAgent);
+console.log(`Resolution: ${width}x${height}`)
